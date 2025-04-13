75146bdf8400cc6d72497a8f92fb5e3fb7faf248
shayaandanishansari <shayaan0303@gmail.com>
2025-04-13 21:20:23 +0500

    Updated Main File with boilerplate code

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
 
+pygame.init()
 
-git
+screen = pygame.display.set_mode()
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if pygame.event.type == pygame.QUIT:
+            pygame.quit()
+    clock.tick(60)
+
 if __name__ == '__main__':
     print("Hello World")
