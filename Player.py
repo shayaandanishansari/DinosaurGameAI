da90075e9549205c85fae174dfb241f31d22e98e
shayaan <shayaan0303@gmail.com>
2025-04-15 15:35:57 +0500

    Updated Player file. Added Assets to git repo.

--- Player.py
+++ Player.py
@@ -15,6 +15,9 @@ class Player(pygame.sprite.Sprite):
         self.jumping = False
         self.ducking = False
 
+        self.start_jump = 0
+        self.jump_duration = 0
+
         self.image = self.run_images[self.current_image_index]
         self.rect = self.image.get_rect()
         self.rect.bottomleft = y
@@ -25,10 +28,17 @@ class Player(pygame.sprite.Sprite):
         start_jump = 0
         end_jump = 0
 
-        if keyboard["spacebar"]:
-            start_jump = pygame.time.get_ticks()
-        else:
-            end_jump = pygame.time.get_ticks() - start_jump
+        # JUMPING
+        # Pressed Jump
+        if keyboard["spacebar"] and not self.jumping:
+            self.jumping = True
+            self.start_jump = pygame.time.get_ticks()
+        # Holding Jump
+        elif self.jumping and keyboard["spacebar"]:
+            self.jump_duration = pygame.time.get_ticks() - self.start_jump
+        elif self.jumping and keyboard["spacebar"] and self.jump_duration < settings.max_jump_time:
+            self.jumping = False
+            self.jump_duration = 0
 
         if keyboard["down"]:
             # duck
